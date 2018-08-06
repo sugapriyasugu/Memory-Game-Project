3a62aba811da2b85329944e24928b6240119511b
suga <sugapriyam21@gmail.com>
2018-08-07 00:02:17 +0530

    start the game

--- js/app.js
+++ js/app.js
@@ -10,3 +10,17 @@ let starsCount = 3;
 startGame();
 deck.addEventListener('click',displayCard);
 document.querySelector('.restart').addEventListener('click',restartGame);
+
+/*
+ * Displays the cards on the page
+ *   - shuffle the list of cards using the "shuffle" method 
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+function startGame() {
+    cards=shuffle(Array.from(cards));
+    deck.innerHTML= "";
+    for (let i=0;i<cards.length;i++) {
+    deck.appendChild(cards[i]);
+    }
+}
